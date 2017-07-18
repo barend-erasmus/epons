0b9e255783b7806918cb93a7cfa7f66eacb72dbd
Barend Erasmus <developersworkspace@gmail.com>
2017-07-18 09:37:27 +0200

    Adds BOC

--- Epons.Domain/EntityViews/CompletedMeasurementTool.cs
+++ Epons.Domain/EntityViews/CompletedMeasurementTool.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Epons.Domain.EntityViews
 {
@@ -9,5 +10,21 @@ namespace Epons.Domain.EntityViews
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public Dictionary<string, int> ScoreItems { get; set; }
+
+        public double CalculateBurdenOfCare()
+        {
+            if (MeasurementTool.Name == "Beta")
+            {
+                double sum = ScoreItems.OrderBy(y => y.Key).Select(y => (double)y.Value).Sum();
+
+                double value = (126 - sum) * 3.38 / 60;
+
+                return Math.Floor(value * 10) / 10;
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
 }
